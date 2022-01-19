30ff42bad4b60258e162613e7ebf6452ea3429b5
rune0018 <rune00nielsen@gmail.com>
2022-01-19 09:56:54 +0100

    initial

---
+++ Menu_Med_Select_Visning/WebSite2/ScriptFiles/MenuBar.js
@@ -0,0 +1,69 @@
+﻿const TopMenubarDivIDName = "TopMenuBarContent";
+const TopMenuBarHTMLPage = window.location.origin + "/Topmenu/TopMenuBar.html";
+const StartupPage = window.location.origin + "/Index.html";
+
+var TopMenuBarContent = $("#" + TopMenubarDivIDName);
+var MenuCurrentItemCSSClassName = "MenuCurrentItem";
+
+var CheckForDropDownClassName = "dropdown-content";
+var MenuCurrentItemParentCSSClassName = "MenuCurrentItemParent";
+
+function CheckIfTopMenuDivExistElseCreateItAndInsert() {
+    if (0 == TopMenuBarContent.length) {
+        var TopmenuBarContentCreate = document.createElement('div');
+        TopmenuBarContentCreate.id = TopMenubarDivIDName;
+
+        $('body')[0].prepend(TopmenuBarContentCreate);
+        TopMenuBarContent = $("#" + TopMenubarDivIDName);
+    }
+}
+
+async function LoadTopMenuContent(Content) {
+    const Response = await fetch(Content);
+    return await Response.text();
+}
+
+async function LoadTopMenu() {
+    const TopMenuBarDiv = document.getElementById(TopMenubarDivIDName);
+    TopMenuBarDiv.innerHTML = await LoadTopMenuContent(TopMenuBarHTMLPage);
+}
+
+//Funktionen herunder bliver ikke brugt.Den er kun medtaget for at vise
+//forskelllige måder at tilgå sider på 
+function FindCurrentPage() {
+    var CurrentURL = document.location.href;
+    var OriginURL = window.location.origin;
+    var test = window.location.href;
+    var test1 = window.location.pathname;
+}
+
+function MakeTopMenuBar() {
+    CheckIfTopMenuDivExistElseCreateItAndInsert();
+
+    LoadTopMenu();
+
+    var CurrentPage;
+
+    try {
+        CurrentPage = window.location.pathname;
+    }
+    catch (err) {
+        CurrentPage = StartupPage;
+    }
+    finally {
+        setTimeout(function () {
+            PageLoadTimeOut(CurrentPage);
+        }, 200);
+    }
+}
+
+function PageLoadTimeOut(CurrentPageInfo) {
+    $("> ul li", TopMenuBarContent).find('a').each(function () {
+        if ($(this).attr('href').toLowerCase() == CurrentPageInfo.toLowerCase()) {
+            $(this).addClass(MenuCurrentItemCSSClassName);
+            if ($(this).parent().hasClass(CheckForDropDownClassName)) {
+                $(this).parent().prev().addClass(MenuCurrentItemParentCSSClassName);
+            }
+        }
+    });
+}
---
+++ Menu_Med_Select_Visning/WebSite2/ScriptFiles/lommeregner.js
@@ -0,0 +1,128 @@
+﻿//lommeregner
+
+let output = null
+let indeks = 0
+let storedvalues = [0,0]
+let lastoberation = ""
+let safeoutput = null
+var results = []
+
+function pressedvalue(value) {
+    safeoutput = safeoutput + value + ""
+    output = output + value + ""
+    storedvalues[indeks] = parseFloat(safeoutput)
+    document.getElementById("output").innerHTML = output
+}
+function clearall(){
+    output = null
+    storedvalues = [0,0]
+    safeoutput = ""
+    indeks = 0
+    document.getElementById("output").innerHTML = output
+}
+function add() {
+    if (lastoberation == "+" && indeks >= 1) {
+        storedvalues[0] = storedvalues[0] + storedvalues[1]
+        storedvalues[1] = 0
+        safeoutput = null
+        output = storedvalues[0] + " + "
+        document.getElementById("output").innerHTML = output
+        return
+    }
+    safeoutput = null
+    output = output + " + "
+    indeks++
+    lastoberation = "+"
+    document.getElementById("output").innerHTML = output
+}
+function subtract() {
+    if (lastoberation == "-" && indeks >= 1) {
+        storedvalues[0] = storedvalues[0] - storedvalues[1]
+        storedvalues[1] = 0
+        safeoutput = null
+        output = storedvalues[0] + " - "
+        document.getElementById("output").innerHTML = output
+        return
+    }
+    safeoutput = null
+    output = output + " - "
+    indeks++
+    lastoberation = "-"
+    document.getElementById("output").innerHTML = output
+}
+function multiply() {
+    if (lastoberation == "*" && indeks >= 1) {
+        storedvalues[0] = storedvalues[0] * storedvalues[1]
+        storedvalues[1] = 0
+        safeoutput = null
+        output = storedvalues[0] + " * "
+        document.getElementById("output").innerHTML = output
+        return
+    }
+    safeoutput = null
+    output = output + " * "
+    indeks++
+    lastoberation = "*"
+    document.getElementById("output").innerHTML = output
+}
+function devide() {
+    if (lastoberation == "/" && indeks >= 1) {
+        storedvalues[0] = storedvalues[0] / storedvalues[1]
+        storedvalues[1] = 0
+        safeoutput = null
+        output = storedvalues[0] + " / "
+        document.getElementById("output").innerHTML = output
+        return
+    }
+    safeoutput = null
+    output = output + " / "
+    indeks++
+    lastoberation = "/"
+    document.getElementById("output").innerHTML = output
+}
+function modulos() {
+    if (lastoberation == "%" && indeks >= 1) {
+        storedvalues[0] = storedvalues[0] % storedvalues[1]
+        storedvalues[1] = 0
+        safeoutput = null
+        output = storedvalues[0] + " % "
+        document.getElementById("output").innerHTML = output
+        return
+    }
+    safeoutput = null
+    output = output + " % "
+    indeks++
+    lastoberation = "%"
+    document.getElementById("output").innerHTML = output
+}
+function equals() {
+    let finalresult = null
+    if (lastoberation == "+") {
+        finalresult = storedvalues[0] + storedvalues[1]
+        output = finalresult + ""
+        storedvalues = [0, 0]
+    } else if (lastoberation == "-") {
+        finalresult = storedvalues[0] - storedvalues[1]
+        output = finalresult + ""
+        storedvalues = [0, 0]
+    } else if (lastoberation == "*") {
+        finalresult = storedvalues[0] * storedvalues[1]
+        output = finalresult + ""
+        storedvalues = [0, 0]
+    } else if (lastoberation == "/") {
+        finalresult = storedvalues[0] / storedvalues[1]
+        output = finalresult + ""
+        storedvalues = [0, 0]
+    } else if (lastoberation == "%") {
+        finalresult = storedvalues[0] % storedvalues[1]
+        output = finalresult + ""
+        storedvalues = [0, 0]
+    }else {
+        return
+    }
+    safeoutput = null
+    document.getElementById("output").innerHTML = output
+    results.push(output)
+    output = ""
+    indeks = 0
+}
